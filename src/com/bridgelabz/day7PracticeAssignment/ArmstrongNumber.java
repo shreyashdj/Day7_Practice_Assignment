efc25c43e3e61532da9eacebd76ad29f8774971a
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-24 09:39:12 +0530

    Added:[Shreyash] program to check input no. is Armstrong number or not

---
+++ src/com/bridgelabz/day7PracticeAssignment/ArmstrongNumber.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.day7PracticeAssignment;
+
+import java.util.Scanner;
+/*
+ * @author SHREYASH JADHAV
+ */
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+
+		// variables
+		int lastDigit;
+		int sum = 0;
+		int digits = 0;
+		
+		Scanner scan = new Scanner(System.in);		// created object of Scanner class
+		
+		System.out.print("Enter a number : ");
+		int number = scan.nextInt();	// taking input of number
+		int num = number;
+		
+		//	calculating number of digits
+		while (num > 0) {
+			num /=  10;
+			digits++;
+		}
+		
+		//	sum of all digits raised to the power of total no. of digits
+		num = number;
+		while (num > 0) {
+			lastDigit = num % 10;		// separating last digit
+			sum += Math.pow(lastDigit,digits);
+			num /= 10;
+		}
+		
+		//	checking entered number is Armstrong no. or Not
+		if (number == sum) {
+			System.out.println(number +" is Armstrong number");
+		} else {
+			System.out.println(number +" is not Armstrong number");
+		}
+		
+scan.close();
+	}
+}
